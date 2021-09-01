fa9552abd1da3ba00974584e2cad847421851ecf
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-09-01 13:29:08 +0200

    Keinen Plan

--- app/db.py
+++ app/db.py
@@ -4,5 +4,19 @@ from app import app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://chronos.db'
 db = SQLAlchemy(app)
 
-class Test(db.Model):
-    pass
+
+class nutzer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nname = db.Column(db.String(50))
+    vname = db.Column(db.String(50))
+    nutzername = db.Column(db.String(10), unique=True)
+    email = db.Column(db.String(50))
+    passwort = db.Column(db.String(50))
+    kartennr = db.Column(db.String(50), unique=True)
+    benutzerStatus = db.Column(db.String(10), default='abwesend')
+    
+
+class buchung(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    buchungArt = db.Column(db.String(10),)
+    n_kartennr = db.Column(db.String(50), db.ForeignKey('nutzer.id'))
