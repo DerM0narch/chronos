2fccbd95ca145b4ceab539cdc03e54440634886b
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-09-02 14:20:45 +0200

    Nutzer hinzufügen

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from . import db
+from .models import Nutzer, Buchung
 
 views = Blueprint('views', __name__)
 
@@ -9,13 +10,28 @@ def index():
     return render_template('index.html')
 
 @views.route("/nutzer")
-def nutzer():
+def user():
     return render_template('user.html')
 
 @views.route("/uebersicht")
 def uebersicht():
     return render_template('uebersicht.html')
 
-@views.route('/nutzeranlegen')
+@views.route('/nutzeranlegen', methods=["GET", "POST"])
 def nutzeranlegen():
+    if request.method=="POST":
+        A_nname = request.form.get('nname')
+        A_vname = request.form.get('vname')
+        A_kuerzel = A_nname[:4] + A_vname[:3]
+        A_email = request.form.get('email')
+        A_passwort = request.form.get('passwort')
+        A_kartennr = request.form.get('kartennr')
+
+        benutzer = Nutzer(nname=A_nname, vname=A_vname, nutzername=A_kuerzel,
+                          email=A_email, passwort=A_passwort, 
+                          kartennr=A_kartennr)
+
+        db.session.add(benutzer)
+        db.session.commit()
+
     return render_template('nutzerAnlegen.html')
