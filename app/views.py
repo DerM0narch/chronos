95e106c7ca3d754f39b8eb0019336031f0c833b3
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-09-02 13:49:40 +0200

    Pfade hinzugefügt

--- app/views.py
+++ app/views.py
@@ -7,3 +7,15 @@ views = Blueprint('views', __name__)
 @views.route("/")
 def index():
     return render_template('index.html')
+
+@views.route("/nutzer")
+def nutzer():
+    return render_template('user.html')
+
+@views.route("/uebersicht")
+def uebersicht():
+    return render_template('uebersicht.html')
+
+@views.route('/nutzeranlegen')
+def nutzeranlegen():
+    return render_template('nutzerAnlegen.html')
