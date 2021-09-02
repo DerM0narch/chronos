3f42b404ad7d01a14e8dbd09cca2a1fc93cda0a8
Hoff1GGOD <lars.hofmann0902@outlook.com>
2021-09-02 11:26:52 +0200

    hizufügen von routen

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,15 @@ from app import app
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/nutzer")
+def nutzer():
+    return render_template('user.html')
+
+@app.route("/uebersicht")
+def uebersicht():
+    return render_template('uebersicht.html')
+
+@app.route('/nutzeranlegen')
+def nutzeranlegen():
+    return render_template('nutzerAnlegen.html')
