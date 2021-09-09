94cc69430230b8785d0d9de6f5aebbded1fb8e8b
DerM0narch <p.lokau@web.de>
2021-09-09 09:39:22 +0200

    gitignore fix

---
+++ app/__init__.py
@@ -0,0 +1,61 @@
+from flask import Flask, redirect
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy, event
+from flask_login import LoginManager, current_user
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from os import path
+import os
+
+db = SQLAlchemy()
+DB_NAME = "chronos.db"
+
+
+def create_app():
+    
+    # App Setup
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "Communism"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
+    # Routes Blueprints
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    
+    # Database
+    from .models import Nutzer, Buchung
+    
+    create_database(app)
+    
+    # Login Managing
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return Nutzer.query.get(int(id))
+    
+    # Admin Panel
+    class NutzerView(ModelView):
+        can_create = False
+        can_edit = False
+        column_display_pk = True
+        column_list = ['id', 'nname', 'vname','nutzername', 'email', 'kartennr']
+        column_labels = dict(id='ID', nname='Nachname', vname='Vorname', nutzername='Kuerzel', email='E-Mail', kartennr='Kartennummer')
+        column_searchable_list = ('nname', 'vname', 'email', 'kartennr')
+        
+    admin = Admin(app)
+    admin.add_view(NutzerView(Nutzer, db.session))
+    admin.add_view(ModelView(Buchung, db.session))
+    
+    return app
+    
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
---
+++ app/auth.py
@@ -0,0 +1,30 @@
+from . import db
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import check_password_hash
+from flask_login import login_required, login_user, logout_user, current_user
+from .models import Nutzer
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('user')
+        passwort = request.form.get('password')
+        
+        nutzer = Nutzer.query.filter_by(nutzername=username).first()
+        
+        if nutzer:
+            if check_password_hash(nutzer.passwort, passwort):
+                flash("Logged in", "succesful")
+                login_user(nutzer, remember=True)
+                return redirect(url_for("views.startseite", user=current_user))
+
+    return render_template('index.html', user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
