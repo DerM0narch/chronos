2c918aea682e0972561fa44a302f1313eb062ab3
DerM0narch <p.lokau@web.de>
2021-09-10 12:19:07 +0200

    WIP - usage of sqlite3

--- joypi/main.py
+++ joypi/main.py
@@ -1,37 +1,51 @@
+from app import DB_NAME
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as gpio
+import sqlite3 as sql
+from sqlite3 import Error
 
-import sys
-sys.path.append("..")
-from app import db
-from app.models import Nutzer, Buchung
+DB_NAME = "chronos.db"
+DB_FILE = F'app/{DB_NAME}'
 
 
 reader = SimpleMFRC522()
 
 def RFIDread():
+    con = sql.create_connection(DB_FILE)
     gpio.setwarnings(False)
+    
     while True:
         try:
             kartenid = reader.read()
         finally:
             gpio.cleanup()
         
-        rfUser = Nutzer.query.filter_by(kartennr=kartenid).first()
+        with con:
+            
+            cur = con.cursor()
+            
+            cur.execute(F"SELECT benutzerstatus from Nutzer where kartennr={kartenid}")
+            result = cur.fetchone()
+            
+            if result == "abwesend" or result == "in Pause":
+                pass
+            elif result == "anwesend":
+                pass
         
-        if rfUser.benutzerStatus == "anwesend":
-            rfUser.benutzerStatus = 'abwesend'
-            
-            buchung = Buchung("gehen", rfUser.kartennr)
-            db.session.add(buchung)
-            db.session.commit()
-        elif  rfUser.benutzerStatus == "abwesend" or rfUser.benutzerStatus == "in Pause":
-            rfUser.benutzerStatus = 'anwesend'
-            
-            buchung = Buchung("kommen", rfUser.kartennr)
-            db.session.add(buchung)
-            db.session.commit()
-    
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sql.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
     
 if __name__ == "__main__":
     RFIDread()
