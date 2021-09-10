39d3db62cd50ab89b9ab314c87172fdc569628f1
DerM0narch <p.lokau@web.de>
2021-09-10 10:25:13 +0200

    Update main.py

--- joypi/main.py
+++ joypi/main.py
@@ -10,25 +10,26 @@ from app.models import Nutzer, Buchung
 reader = SimpleMFRC522()
 
 def RFIDread():
-    try:
-        kartenid = reader.read()
-    finally:
-        gpio.cleanup()
-    
-    rfUser = Nutzer.query.filter_by(kartennr=kartenid).first()
-    
-    if rfUser.benutzerStatus == "anwesend":
-        rfUser.benutzerStatus = 'abwesend'
+    while True:
+        try:
+            kartenid = reader.read()
+        finally:
+            gpio.cleanup()
         
-        buchung = Buchung("gehen", rfUser.kartennr)
-        db.session.add(buchung)
-        db.session.commit()
-    elif  rfUser.benutzerStatus == "abwesend" or rfUser.benutzerStatus == "in Pause":
-        rfUser.benutzerStatus = 'anwesend'
+        rfUser = Nutzer.query.filter_by(kartennr=kartenid).first()
         
-        buchung = Buchung("kommen", rfUser.kartennr)
-        db.session.add(buchung)
-        db.session.commit()
+        if rfUser.benutzerStatus == "anwesend":
+            rfUser.benutzerStatus = 'abwesend'
+            
+            buchung = Buchung("gehen", rfUser.kartennr)
+            db.session.add(buchung)
+            db.session.commit()
+        elif  rfUser.benutzerStatus == "abwesend" or rfUser.benutzerStatus == "in Pause":
+            rfUser.benutzerStatus = 'anwesend'
+            
+            buchung = Buchung("kommen", rfUser.kartennr)
+            db.session.add(buchung)
+            db.session.commit()
     
     
     
